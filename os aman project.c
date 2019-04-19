74d6b18a55bc652a535c0949841a185903cf266b
Aman2812 <49606294+Aman2812@users.noreply.github.com>
2019-04-19 21:55:39 +0530

    Add files via upload

---
+++ os aman project.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+ int main()
+ { 	
+ double pg_flt;	
+ double avg_t;	
+ double memory_acc_t; 	
+ double modi_per;	
+ double t_n; 	
+ double t_m; 
+ 
+ printf(" ----Demand Paged Memory Problem---- \n\n"); 
+ printf("IN this problem take the values given below or you can take your own values! \n\n"); 
+ 
+ printf("Average time = 200 | Memory Access Time = 100 | Modification Percentage = 70%%|"); 
+ printf(" Time_n = 8 | Time_m = 20\n\n");
+ printf("Average time :"); 
+ scanf("%lf",&avg_t); 
+ printf("\nMemory Access time :"); 
+ scanf("%lf",&memory_acc_t); 
+ printf("\nModification percentage :"); 
+ scanf("%lf",&modi_per); 
+ modi_per /= 100; 
+ printf("\nTime when not modified(Time_n):"); 
+ scanf("%lf",&t_n); 
+ t_n *= 1000000;
+ printf("\nTime when modified(Time_m):"); 
+ scanf("%lf",&t_m); 
+ t_m *= 1000000;
+ pg_flt = (avg_t - memory_acc_t)/(( modi_per*(t_m)+(1 - modi_per)*(t_n)) - memory_acc_t); 
+ printf("\nThe maximum acceptable page-fault rate for an effective access time : %lf%%\n\n",pg_flt);
+} 
+ 
+ 
+
